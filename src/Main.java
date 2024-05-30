2eec817b010219503faf1707509d4bc98c6fabf2
Victor-jvas <victor.jvas1@gmail.com>
2024-05-29 23:56:23 -0300

    Challenge complete

---
+++ src/Main.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int parametroUm, parametroDois;
+
+
+        System.out.println("Digite o primeiro parâmetro");
+        parametroUm = sc.nextInt();
+
+        System.out.println("Digite o segundo parâmetro");
+        parametroDois = sc.nextInt();
+
+        try {
+            contar(parametroUm, parametroDois);
+        } catch (ParametrosInvalidosException e) {
+            System.out.println("O segundo parâmetro deve ser maior que o primeiro!");
+            System.out.println(e);
+        }
+
+    }
+
+    private static void contar(int parametroUm, int parametroDois) throws ParametrosInvalidosException {
+
+        if (parametroUm > parametroDois) {
+            throw new ParametrosInvalidosException();
+        }
+
+        int contagem = parametroDois - parametroUm;
+
+        for (int i = 0; i < contagem; i++) {
+            System.out.printf("Imprimindo o número %d\n", i+1);
+        }
+    }
+}
